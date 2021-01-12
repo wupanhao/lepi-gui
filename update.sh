93ae1b8eca3c85208d512b6081d5fe95ff34f1ed
Panhao Wu <wupanhao@qq.com>
2021-01-12 06:54:31 +0000

    update script modified

--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
+source ~/nodejs.sh
 cd ~/workspace/lepi-gui/app && npm i
 cd ~/workspace/lepi-gui/server && npm i
+source ~/workspace/lepi-gui/env.sh && cd ~/workspace/lepi-ros-server/catkin_ws && catkin_make
